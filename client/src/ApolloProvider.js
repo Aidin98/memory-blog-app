9f106b432d7588a5428f09800171170a5154ca83
Aidin98git config --global user.email aidin.basic@edu.fit.baaidin@DESKTOP-KV6E9PU MINGW64 ~/OneDrive/Desktop/web d/memory-server (master)$ git initReinitialized existing Git repository in C:/Users/aidin/OneDrive/Desktop/web d/memory-server/.git/aidin@DESKTOP-KV6E9PU MINGW64 ~/OneDrive/Desktop/web d/memory-server (master)$ git add .aidin@DESKTOP-KV6E9PU MINGW64 ~/OneDrive/Desktop/web d/memory-server (master)$ git commit -m init[master (root-commit) ed00cbc] init 13 files changed, 3554 insertions(+) create mode 100644 .gitignore create mode 100644 graphql/resolvers/comments.js create mode 100644 graphql/resolvers/index.js create mode 100644 graphql/resolvers/posts.js create mode 100644 graphql/resolvers/users.js create mode 100644 graphql/typeDefs.js create mode 100644 index.js create mode 100644 models/Post.js create mode 100644 models/User.js create mode 100644 package-lock.json create mode 100644 package.json create mode 100644 util/check-auth.js create mode 100644 util/validators.jsaidin@DESKTOP-KV6E9PU MINGW64 ~/OneDrive/Desktop/web d/memory-server (master)$ git remote add origin git@github.com:Aidin98/memory-blog-server.gitaidin@DESKTOP-KV6E9PU MINGW64 ~/OneDrive/Desktop/web d/memory-server (master)$ git remote -vorigin  git@github.com:Aidin98/memory-blog-server.git (fetch)origin  git@github.com:Aidin98/memory-blog-server.git (push)aidin@DESKTOP-KV6E9PU MINGW64 ~/OneDrive/Desktop/web d/memory-server (master)$ git push origin masterThe authenticity of host 'github.com (140.82.121.3)' can't be established.RSA key fingerprint is SHA256:nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8.Are you sure you want to continue connecting (yes/no/[fingerprint])? yPlease type 'yes', 'no' or the fingerprint: yesWarning: Permanently added 'github.com,140.82.121.3' (RSA) to the list of known hosts.git@github.com: Permission denied (publickey).fatal: Could not read from remote repository.Please make sure you have the correct access rightsand the repository exists.aidin@DESKTOP-KV6E9PU MINGW64 ~/OneDrive/Desktop/web d/memory-server (master)$ git logingit: 'login' is not a git command. See 'git --help'.The most similar command is        columnaidin@DESKTOP-KV6E9PU MINGW64 ~/OneDrive/Desktop/web d/memory-server (master)$ git config --global user.name Aidin98 <78453568+Aidin98@users.noreply.github.com>
2021-04-11 22:38:37 +0200

    deploy

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: "https://nameless-meadow-33950.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
